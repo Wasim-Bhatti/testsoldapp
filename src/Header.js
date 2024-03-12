7337c61a2e433be765f28f7ef37627723392e277
wasim-bhatti <wasimbhatti077@gmail.com>
2024-03-12 03:05:10 -0230

    Updated Header and Background color + header changes

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import './Header.css';
 
 function Header() {
   return (
-    <header>
-      <h1>Welcome to my Solana DApp!</h1>
+    <header className="header">
+      <div className="logo-container">
+        <img src="logo.png" alt="logo" className="logo-image" /> {/* Replace "logo.png" with your actual logo image path */}
+        <h1 className="logo">WriteGather</h1>
+      </div>
+      <nav>
+        <ul className="nav-links">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Mission</a></li>
+          <li><a href="#">Roadmap</a></li>
+          <li><a href="#" className="sign-up">Sign Up</a></li>
+        </ul>
+      </nav>
     </header>
   );
 }
